2b3c25344eccaa4f5ddf78358d1c25fe8c98d875
igormomcilovic <igormo@hotmail.com>
2022-02-27 15:14:46 +0100

    Added animation

--- Collatz.py
+++ Collatz.py
@@ -1,51 +1,51 @@
 import matplotlib.pyplot as plt
-import numpy as np; np.random.seed(2)
+import numpy as np;
 
-def MakeGraph():
-    tall = int(input("Give me a number: "))
-    print(tall)
-    x = []
-    y = []
-    counter = 1
+np.random.seed(2)
 
+tall = int(input("Give me a number: "))
+print(tall)
+x = []
+y = []
+counter = 1
+
+x.append(tall)
+y.append(counter)
+while tall != 1:
+    if (tall % 2 == 0):
+        tall = tall // 2
+        counter += 1
+        y.append(counter)
+        print(tall)
+    elif (tall % 2 != 0):
+        tall = tall * 3 + 1
+        counter += 1
+        y.append(counter)
+        print(tall)
     x.append(tall)
-    y.append(counter)
-    while tall != 1:
-        if(tall %2 == 0):
-            tall = tall // 2
-            counter+=1
-            y.append(counter)
-            print(tall)
-        elif(tall %2 != 0):
-            tall = tall * 3 + 1
-            counter+=1
-            y.append(counter)
-            print(tall)
-        x.append(tall)
+
+print("steps to 1: " + str(counter))
 
 
-    print("steps to took to find 1: " + str(counter) + " Steps")
-
-    
-    plt.plot(y, x,color='red', linestyle='dashed', linewidth = 1,
-            marker='o', markerfacecolor='blue', markersize=7)
-            
-            
-    
-    # setting x and y axis range
-    plt.ylim(1,max(x) + 5)
-    plt.xlim(1,counter + 5)
-    
-    # naming the x axis
-    plt.xlabel('Steps')
-    # naming the y axis
-    plt.ylabel('Number Value')
-    
-    # giving a title to my graph
-    plt.title('Collatz Conjecture')
-    
-    # function to show the plot
+def animate_plot_with_lines():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_ylim([0, max(y)])
+    ax.set_xlim([0, max(x)])
+    ax.set_xlabel('Number')
+    ax.set_ylabel('Steps')
+    ax.set_title('Collatz')
+    if counter > 30:
+        line, = ax.plot(x, y, 'r-')
+    else:
+        line, = ax.plot(x, y, 'bo-')
+    for i in range(len(x)):
+        line.set_xdata(x[:i])
+        line.set_ydata(y[:i])
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+        plt.pause(0.1)
     plt.show()
-    return
-    
-MakeGraph()
+
+
+animate_plot_with_lines()
